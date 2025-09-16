1e58bf14e007088ac56511d4087c4c3553114ad8
CJ Wang <lejacp@gmail.com>
2025-09-16 23:37:44 +0800

    add product model and post method

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,28 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
+import Product from './models/product.model.js';
 
 dotenv.config();
 const app = express();
+app.use(express.json()); // allow us to accept json data in body
 
-app.get('/', (req, res) => {
-    res.send('Server is running');
+app.post('/api/products', async (req, res) => {
+    const product = req.body;
+
+    if (!product.name || !product.price || !product.image) {
+        return res.status(400).json({ success:false, message: 'Please provide all fields'});
+    }
+
+    const newProduct = new Product(product);
+
+    try {
+        await newProduct.save();
+        res.status(201).json({ success:true, data: newProduct });
+    } catch (error) {
+        console.error("Error in Create Product:", error.message);
+        res.status(500).json({ success:false, message: 'Server Error' });
+    }
 });
 
 
