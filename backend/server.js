e64f131d6b68a0c83cc14c57fc3df801775f5b11
CJ Wang <lejacp@gmail.com>
2025-09-17 23:43:12 +0800

    build api and separate to route and controller

--- backend/server.js
+++ backend/server.js
@@ -1,30 +1,13 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
-import Product from './models/product.model.js';
+import productRoutes from './routes/product.route.js';
 
 dotenv.config();
 const app = express();
 app.use(express.json()); // allow us to accept json data in body
 
-app.post('/api/products', async (req, res) => {
-    const product = req.body;
-
-    if (!product.name || !product.price || !product.image) {
-        return res.status(400).json({ success:false, message: 'Please provide all fields'});
-    }
-
-    const newProduct = new Product(product);
-
-    try {
-        await newProduct.save();
-        res.status(201).json({ success:true, data: newProduct });
-    } catch (error) {
-        console.error("Error in Create Product:", error.message);
-        res.status(500).json({ success:false, message: 'Server Error' });
-    }
-});
-
+app.use("/api/products", productRoutes);
 
 app.listen(5000, () => {
     connectDB();
