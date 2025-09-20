044605f5c3f869045e512c38c965a7f093760ee8
CJ Wang <lejacp@gmail.com>
2025-09-20 17:03:15 +0800

    fix error messages

--- backend/server.js
+++ backend/server.js
@@ -5,11 +5,12 @@ import productRoutes from './routes/product.route.js';
 
 dotenv.config();
 const app = express();
+const PORT = process.env.PORT || 5000;
 app.use(express.json()); // allow us to accept json data in body
 
 app.use("/api/products", productRoutes);
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
     connectDB();
-    console.log('Server is running on http://localhost:5000');
+    console.log('Server is running on http://localhost:' + PORT);
 });
